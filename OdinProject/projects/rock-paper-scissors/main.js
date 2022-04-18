be2211af1de6e64dc507aa06ce1325cb17740516
CoolHwhipMike <coolhwhipmike@gmail.com>
2022-04-18 13:20:29 -0400

    rps-ui is functional. added some styling but will have to come back later. I dont want to waste time styling rather than learning right now.

--- OdinProject/projects/rock-paper-scissors/main.js
+++ OdinProject/projects/rock-paper-scissors/main.js
@@ -1,111 +1,107 @@
+const modal = document.querySelector(".modal");
+const openModal = document.querySelector(".openModal");
+const closeModal = document.querySelector(".closeModal");
 
-const btnRock = document.querySelector("#btnRock");
+openModal.addEventListener("click", () => modal.showModal());
+closeModal.addEventListener("click", () => modal.close());
+
+// Initialization and getting input
+let playerScore = 0;
+let computerScore = 0;
+let roundCount = 0;
+
+const playerScoreOutput = document.querySelector(".playerScoreOutput");
+const computerScoreOutput = document.querySelector(".computerScoreOutput");
+const roundCountOutput = document.querySelector(".roundCountOutput");
+const userPlayOutput = document.querySelector(".userPlayOutput");
+const computerPlayOutput = document.querySelector(".computerPlayOutput");
+
+const btnRock = document.querySelector(".btnRock");
+const btnPaper = document.querySelector(".btnPaper");
+const btnScissors = document.querySelector(".btnScissors");
+
 btnRock.addEventListener("click", () => {
-    playRound("rock", computerPlay());
+    execGame("rock");
 });
-
-const btnPaper = document.querySelector("#btnPaper");
 btnPaper.addEventListener("click", () => {
-    playRound("paper", computerPlay());
+    execGame("paper");
 });
-
-const btnScissors = document.querySelector("#btnScissors");
 btnScissors.addEventListener("click", () => {
-    playRound("scissors", computerPlay());
+    execGame("scissors");
 });
 
-const divResults = document.querySelector("#divResults");
+// Menu buttons
+const btnReset = document.querySelector(".btnReset");
 
-let playerScoreNum = 0;
-let computerScoreNum = 0;
-const divScore = document.querySelector("#divScore");
-const playerScore = document.createElement("p");
-playerScore.textContent = `Your score is ${playerScoreNum}`;
-divScore.appendChild(playerScore);
+btnReset.addEventListener("click", () => gameReset());
 
+// A function that resets the game.
+function gameReset() {
+    playerScore = 0;
+    computerScore = 0;
+    roundCount = 0;
 
-const computerScore = document.createElement("p");
-computerScore.textContent = `The computer score is ${computerScoreNum}`;
-divScore.appendChild(computerScore);
-
-
-
+    computerScoreOutput.textContent = computerScore;
+    playerScoreOutput.textContent = playerScore;
+    roundCountOutput.textContent = roundCount;
+    userPlayOutput.textContent = "-";
+    computerPlayOutput.textContent = "-";
+}
 
 // A function that picks a word at random for the computer opponent.
 function computerPlay() {
     const choices = ["rock", "paper", "scissors"];
-    const rand = Math.floor(Math.random() * choices.length);
-
-    return choices[rand];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
-// A function that determines the round winner.
+// A function that determines the game winner
+function gameWinner() {
+    if (playerScore >= 5) {
+        alert("You beat the computer!");
+        playerScore = 0;
+        computerScore = 0;
+        roundCount = 0;
+        gameReset();
+    }
+    if (computerScore >= 5) {
+        alert("The computer beat you!");
+        playerScore = 0;
+        computerScore = 0;
+        roundCount = 0;
+        gameReset();
+    }
+}
+
+// A function to determine the round winner
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        const tieText = document.createElement("p");
-        tieText.textContent = "It's a tie!";
-        divResults.appendChild(tieText);
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        const winText = document.createElement("p");
-        winText.setAttribute("style", "background-color: green;");
-        winText.textContent = "You win! Rock beats Scissors.";
-        divResults.appendChild(winText);
-        playerScoreNum += 1;
-        playerScore.textContent = `Your score is ${playerScoreNum}`;
-        divScore.appendChild(playerScore);
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        const winText = document.createElement("p");
-        winText.setAttribute("style", "background-color: green;");
-        winText.textContent = "You win! Paper beats Rock.";
-        divResults.appendChild(winText);
-        playerScoreNum += 1;
-        playerScore.textContent = `Your score is ${playerScoreNum}`;
-        divScore.appendChild(playerScore);
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        const winText = document.createElement("p");
-        winText.setAttribute("style", "background-color: green;");
-        winText.textContent = "You win! Scissors beats Paper.";
-        divResults.appendChild(winText);
-        playerScoreNum += 1;
-        playerScore.textContent = `Your score is ${playerScoreNum}`;
-        divScore.appendChild(playerScore);
-    } else if (computerSelection === "rock" && playerSelection === "scissors") {
-        const loseText = document.createElement("p");
-        loseText.setAttribute("style", "background-color: red;");
-        loseText.textContent = "You Lose! Rock beats Scissors.";
-        divResults.appendChild(loseText);
-        computerScoreNum += 1;
-        computerScore.textContent = `The computer score is ${computerScoreNum}`;
-        divScore.appendChild(computerScore);
-    } else if (computerSelection === "paper" && playerSelection === "rock") {
-        const loseText = document.createElement("p");
-        loseText.setAttribute("style", "background-color: red;");
-        loseText.textContent = "You Lose! Paper beats Rock.";
-        divResults.appendChild(loseText);
-        computerScoreNum += 1;
-        computerScore.textContent = `The computer score is ${computerScoreNum}`;
-        divScore.appendChild(computerScore);
-    } else if (computerSelection === "scissors" && playerSelection === "paper") {
-        const loseText = document.createElement("p");
-        loseText.setAttribute("style", "background-color: red;");
-        loseText.textContent = "You Lose! Scissors beats Paper.";
-        divResults.appendChild(loseText);
-        computerScoreNum += 1;
-        computerScore.textContent = `The computer score is ${computerScoreNum}`;
-        divScore.appendChild(computerScore);
+    if (playerSelection === computerSelection) return null;
+    if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")) return true;
+    return false;
+}
+
+// A function that plays one round of rps
+function execGame(userClick) {
+    const computer = computerPlay();
+    const result = playRound(userClick, computer);
+
+    roundCount += 1;
+    roundCountOutput.textContent = roundCount;
+
+    userPlayOutput.textContent = userClick;
+    computerPlayOutput.textContent = computer;
+
+
+    if (result === true) {
+        playerScore += 1;
+        playerScoreOutput.textContent = playerScore;
     };
 
-    if (playerScoreNum >= 5) {
-        alert("You won the game!");
-        playerScoreNum = 0;
-        computerScoreNum = 0;
-        playerScore.textContent = `The computer score is ${computerScoreNum}`;
-        computerScore.textContent = `The computer score is ${computerScoreNum}`;
-    } else if (computerScoreNum >= 5) {
-        alert("The computer won the game.");
-        playerScoreNum = 0;
-        computerScoreNum = 0;
-        playerScore.textContent = `The computer score is ${computerScoreNum}`;
-        computerScore.textContent = `The computer score is ${computerScoreNum}`;
-    }
-};
+    if (result === false) {
+        computerScore += 1;
+        computerScoreOutput.textContent = computerScore;
+    };
+
+    gameWinner();
+}
