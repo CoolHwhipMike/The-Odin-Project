c5097cf5b72cf2b55e04cc01a95bb2f7df2db1e1
CoolHwhipMike <coolhwhipmike@gmail.com>
2022-04-21 13:48:50 -0400

    Worked through findTheOldest exercise. reduce is confusing to me and I need to practice that more.

--- Foundations/js/fundamentals-part-5/12_findTheOldest/findTheOldest.js
+++ Foundations/js/fundamentals-part-5/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+// const findTheOldest = function (people) {
+//     const oldest = people.sort((a, b) => {
+//         const lastPerson = a.yearOfDeath - a.yearOfBirth;
+//         const nextPerson = b.yearOfDeath - b.yearOfBirth;
+//         let oldestPerson = (lastPerson > nextPerson) ? a : b;
+//         // console.log(lastPerson);
+//         // console.log(nextPerson);
+//         // console.log(oldestPerson.name);
+//         return oldestPerson.name;
+//     });
+// };
 
+const findTheOldest = function (array) {
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
+};
+
+const getAge = function (brith, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - brith;
 };
 
 // Do not edit below this line
