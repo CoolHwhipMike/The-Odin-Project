ad0f43ddadbf2635b30f751e9848dc4191ae421d
CoolHwhipMike <coolhwhipmike@gmail.com>
2022-04-21 12:31:43 -0400

    Completed Fibonacci exercise.

--- Foundations/js/fundamentals-part-5/10_fibonacci/fibonacci.js
+++ Foundations/js/fundamentals-part-5/10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (numberOfDigits) {
+    if (numberOfDigits < 0) return "OOPS";
 
+    let count = 0;
+    let previousNumber = 1;
+    let currentNumber = 0;
+    let nextNumber = 0;
+    while (count < numberOfDigits) {
+        nextNumber = currentNumber + previousNumber;
+        previousNumber = currentNumber;
+        currentNumber = nextNumber;
+        count++;
+    }
+    return currentNumber;
 };
 
 // Do not edit below this line
