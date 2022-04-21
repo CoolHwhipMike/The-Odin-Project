609cf3dc97f747bf11578bb3bcc6fb432db49452
CoolHwhipMike <coolhwhipmike@gmail.com>
2022-04-21 12:09:57 -0400

    Completed calculator exercise.

--- Foundations/js/fundamentals-part-5/08_calculator/calculator.js
+++ Foundations/js/fundamentals-part-5/08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arg) {
+  const totalSum = arg.reduce((total, digit) => {
+    return total + digit;
+  }, 0);
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function (arg) {
+  const totalProduct = arg.reduce((total, digit) => {
+    return total * digit;
+  }, 1);
+  return totalProduct;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (arg) {
+  let total = 1;
+  if (arg === 0) return total;
+
+  while (arg > 0) {
+    total *= arg;
+    arg--;
+  };
+  return total;
 };
 
 // Do not edit below this line
