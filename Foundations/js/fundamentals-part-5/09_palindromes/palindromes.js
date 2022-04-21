30a1a3451352db15e0ff46cddae6e4fe898a2d68
CoolHwhipMike <coolhwhipmike@gmail.com>
2022-04-21 12:10:17 -0400

    Completed palindrome exercise

--- Foundations/js/fundamentals-part-5/09_palindromes/palindromes.js
+++ Foundations/js/fundamentals-part-5/09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const punctuation = /[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    const forward = str.toLowerCase().replace(punctuation, '');
+    const reverse = forward.split("").reverse().join("");
+    return (forward === reverse) ? true : false;
 };
 
 // Do not edit below this line
