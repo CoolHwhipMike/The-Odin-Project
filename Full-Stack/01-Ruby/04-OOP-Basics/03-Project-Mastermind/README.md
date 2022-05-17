60a015cc99f556fe2d85502551e4208b08218a23
CoolHwhipMike <coolhwhipmike@gmail.com>
2022-05-16 22:42:49 -0400

    feat: add basic game logic
    This adds the basic logic to randomly create a code and iterate through 12 guess attempts. No classes or modules have been implementd yet.

--- Full-Stack/01-Ruby/04-OOP-Basics/03-Project-Mastermind/README.md
+++ Full-Stack/01-Ruby/04-OOP-Basics/03-Project-Mastermind/README.md
@@ -3,9 +3,11 @@
 ## Background
 A code is created by one player. The other play tries to guess the code. Feedback is provided if if a correct character is in the wrong position or a correct character is in the correct position. (A modern example is Wordle)
 
-# Goals
+## Goals
 - Player gets 12 attempts.
 - Computer generates a random code
 - Provide guess feedback
 - Refactor to allow human to generate code or guess code
 - Make the computer guess the code
+
+## Pseudocode
--- Full-Stack/01-Ruby/04-OOP-Basics/03-Project-Mastermind/mastermind.rb
+++ Full-Stack/01-Ruby/04-OOP-Basics/03-Project-Mastermind/mastermind.rb
@@ -1,1 +1,41 @@
 # frozen_string_literal: true
+
+def generate_code
+  colors = %w[r g y b m c]
+  code = []
+
+  4.times { code.push(colors.sample) }
+  code
+end
+
+def check_guess
+  current_code = generate_code
+  current_guess = []
+  rounds = 12
+  ##################################### this works but is junk
+  4.times { current_guess.push(gets.chomp) }
+
+  p current_code
+  p current_guess
+  while rounds.positive?
+
+    break if current_guess == current_code
+
+    current_guess.each_with_index do |element, index|
+      if element == current_code[index]
+        puts 'correct'
+      elsif current_code.any?(element)
+        puts 'wrong position'
+      else
+        puts 'wrong'
+      end
+    end
+
+    current_guess.clear
+    4.times { current_guess.push(gets.chomp) }
+    rounds -= 1
+
+  end
+end
+
+check_guess
