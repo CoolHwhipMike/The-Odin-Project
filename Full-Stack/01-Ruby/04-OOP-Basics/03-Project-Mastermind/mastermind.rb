b12298fe139250d852a6fa749582f6dd9313358f
CoolHwhipMike <coolhwhipmike@gmail.com>
2022-05-19 16:45:58 -0400

    Completed matermind - Need to add code maker role Need to fix output for multiple occurances of a color. Will open gh issues and come back to this.

--- Full-Stack/01-Ruby/04-OOP-Basics/03-Project-Mastermind/mastermind.rb
+++ Full-Stack/01-Ruby/04-OOP-Basics/03-Project-Mastermind/mastermind.rb
@@ -1,44 +1,8 @@
 # frozen_string_literal: true
 
 module Mastermind
-  # CLass that generates a game and desired players
-  class Game
-    def initialize
-      @player1 = Human.new
-      @player2 = Computer.new
-    end
-
-    def check_guess
-      @current_code = @player2.generate_code
-      @rounds = 12
-      @current_guess = gets.chomp.split('')
-
-      while @rounds.positive?
-        p @current_code
-        p @current_guess
-
-        break if @current_guess == @current_code
-
-        @current_guess.each_with_index do |element, index|
-          if element == @current_code[index]
-            puts 'correct'
-          elsif @current_code.any?(element)
-            puts 'wrong position'
-          else
-            puts 'wrong'
-          end
-        end
-
-        @current_guess.clear
-        @current_guess = gets.chomp.split('')
-        @rounds -= 1
-
-      end
-    end
-  end
-
   # Human player class
-  class Human
+  class Player
     attr_reader :name
 
     def initialize
@@ -51,14 +15,59 @@ module Mastermind
     attr_reader :code
 
     def generate_code
-      colors = %w[r g y b m c]
-      code = []
+      @colors = %w[r g y b m c]
+      @code = []
 
-      4.times { code.push(colors.sample) }
-      code
+      4.times { @code.push(@colors.sample) }
+      @code
+    end
+  end
+
+  # Class that generates a game and desired players
+  class Game
+    def initialize
+      @player1 = Player.new
+      @player2 = Computer.new
+      @current_code = @player2.generate_code
+      @rounds = 12
+    end
+
+    def guess
+      @current_guess = gets.chomp.split('')
+    end
+
+    def check_guess
+      @current_guess.each_with_index do |element, index|
+        if element == @current_code[index]
+          print element
+        elsif @current_code.any?(element)
+          print 'O'
+        else
+          print 'X'
+        end
+      end
+    end
+
+    def show_result
+      if @current_guess == @current_code
+        puts 'You guessed the code!'
+      else
+        puts 'Sorry, you lost.'
+      end
+    end
+
+    def play_round
+      while @rounds.positive?
+        guess
+        check_guess
+        break if @current_guess == @current_code
+
+        puts ''
+        @rounds -= 1
+      end
+      show_result
     end
   end
 end
 
-include Mastermind
-Game.new.check_guess
+Mastermind::Game.new.play_round
