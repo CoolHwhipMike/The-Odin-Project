ff56ee19a8775c6f71ec86a48794ee17c37277bd
CoolHwhipMike <coolhwhipmike@gmail.com>
2022-05-24 17:55:15 -0400

    Followed along with iteration 1. This will be replaced byt Ruby's CSV parser.

--- Full-Stack/01-Ruby/05-Files-and-Serialization/02-event_manager/lib/event_manager.rb
+++ Full-Stack/01-Ruby/05-Files-and-Serialization/02-event_manager/lib/event_manager.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
-puts 'Event Manager Initialized'
+puts 'EventManager initialized.'
+
+lines = File.readlines('event_attendees.csv')
+lines.each_with_index do |line, index|
+  next if index == 0
+  columns = line.split(',')
+  name = columns[2]
+  puts name
+end
