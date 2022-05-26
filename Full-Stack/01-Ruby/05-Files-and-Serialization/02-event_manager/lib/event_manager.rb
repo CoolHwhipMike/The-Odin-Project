004d5592e3b7c50d080c4d9d6d0c39ad277f9601
CoolHwhipMike <coolhwhipmike@gmail.com>
2022-05-25 22:31:34 -0400

    feat: add most popular days

--- Full-Stack/01-Ruby/05-Files-and-Serialization/02-event_manager/lib/event_manager.rb
+++ Full-Stack/01-Ruby/05-Files-and-Serialization/02-event_manager/lib/event_manager.rb
@@ -12,7 +12,7 @@ end
 def clean_homephone(phone_number)
   phone_number.gsub(/[^0-9]/, '')
 
-  if phone_number.length == 11 && number[0] == '1'
+  if phone_number.length == 11 && phone_number[0] == '1'
     phone_number[1..-1]
   elsif phone_number.length == 10
     phone_number
@@ -21,11 +21,26 @@ def clean_homephone(phone_number)
   end
 end
 
-def target_time
+def target_time(contents)
   times = []
 
   contents.each do |row|
     times.push(Time.strptime(row[:regdate], '%m/%d/%y %k:%M').hour)
+  end
+
+  total = times.reduce({}) do |hash, num|
+    hash[num] ||= 0
+    hash[num] += 1
+    hash
+  end
+  total
+end
+
+def target_day(contents)
+  times = []
+
+  contents.each do |row|
+    times.push(Date.strptime(row[:regdate], '%m/%d/%y %k:%M').wday)
   end
 
   total = times.reduce({}) do |hash, num|
@@ -68,16 +83,18 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-template_letter = File.read('form_letter.erb')
-erb_template = ERB.new template_letter
+# template_letter = File.read('form_letter.erb')
+# erb_template = ERB.new template_letter
 
-contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  legislators = legislators_by_zipcode(zipcode)
+# contents.each do |row|
+#   id = row[0]
+#   name = row[:first_name]
+#   zipcode = clean_zipcode(row[:zipcode])
+#   legislators = legislators_by_zipcode(zipcode)
 
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letter(id, form_letter)
-end
+  # form_letter = erb_template.result(binding)
+# 
+  # save_thank_you_letter(id, form_letter)
+# end
+# puts target_day(contents)
+puts target_time(contents)
