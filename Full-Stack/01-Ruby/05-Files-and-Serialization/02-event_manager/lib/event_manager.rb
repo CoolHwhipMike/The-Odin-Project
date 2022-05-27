1936302e257b063d5f38d70af672f8ac9ba42665
CoolHwhipMike <coolhwhipmike@gmail.com>
2022-05-27 13:34:02 -0400

    refactor: improve finding peak days/hours

--- Full-Stack/01-Ruby/05-Files-and-Serialization/02-event_manager/lib/event_manager.rb
+++ Full-Stack/01-Ruby/05-Files-and-Serialization/02-event_manager/lib/event_manager.rb
@@ -10,7 +10,7 @@ def clean_zipcode(zipcode)
 end
 
 def clean_homephone(phone_number)
-  phone_number.gsub(/[^0-9]/, '')
+  phone_number.gsub!(/[^0-9]/, '')
 
   if phone_number.length == 11 && phone_number[0] == '1'
     phone_number[1..-1]
@@ -21,29 +21,8 @@ def clean_homephone(phone_number)
   end
 end
 
-def target_time(contents)
-  times = []
-
-  contents.each do |row|
-    times.push(Time.strptime(row[:regdate], '%m/%d/%y %k:%M').hour)
-  end
-
-  total = times.reduce({}) do |hash, num|
-    hash[num] ||= 0
-    hash[num] += 1
-    hash
-  end
-  total
-end
-
-def target_day(contents)
-  times = []
-
-  contents.each do |row|
-    times.push(Date.strptime(row[:regdate], '%m/%d/%y %k:%M').wday)
-  end
-
-  total = times.reduce({}) do |hash, num|
+def targeted_advertising(register_event)
+  register_event.each_with_object({}) do |num, hash|
     hash[num] ||= 0
     hash[num] += 1
     hash
@@ -53,14 +32,11 @@ end
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-
   begin
     civic_info.representative_info_by_address(
-      address: zip,
-      levels: 'country',
-      roles: %w[legislatorUpperBody legislatorLowerBody]
+      address: zip, levels: 'country', roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
-  rescue
+  rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
@@ -83,18 +59,27 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+register_times = []
+register_days = []
+
 # template_letter = File.read('form_letter.erb')
 # erb_template = ERB.new template_letter
 
-# contents.each do |row|
-#   id = row[0]
-#   name = row[:first_name]
-#   zipcode = clean_zipcode(row[:zipcode])
-#   legislators = legislators_by_zipcode(zipcode)
+contents.each do |row|
+  # id = row[0]
+  # zipcode = clean_zipcode(row[:zipcode])
+  homephone = clean_homephone(row[:homephone])
+  register_times.push(Time.strptime(row[:regdate], '%m/%d/%y %k:%M').hour)
+  register_days.push(Date.strptime(row[:regdate], '%m/%d/%y %k:%M').wday)
+  name = row[:first_name]
+  # legislators = legislators_by_zipcode(zipcode)
 
   # form_letter = erb_template.result(binding)
-# 
+
   # save_thank_you_letter(id, form_letter)
-# end
-# puts target_day(contents)
-puts target_time(contents)
+  puts "#{name} #{homephone}"
+end
+
+puts '##### Most active days and times #####'
+puts targeted_advertising(register_days)
+puts targeted_advertising(register_times)
