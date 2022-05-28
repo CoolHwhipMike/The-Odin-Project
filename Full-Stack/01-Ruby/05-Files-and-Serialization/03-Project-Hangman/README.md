af64e9c675660c46b22bf404d5262236bf3e6045
CoolHwhipMike <coolhwhipmike@gmail.com>
2022-05-28 15:15:21 -0400

    feat: add basic game logic
    The initial game is finished. There is no save function and some other refinments can be made.

--- Full-Stack/01-Ruby/05-Files-and-Serialization/03-Project-Hangman/README.md
+++ Full-Stack/01-Ruby/05-Files-and-Serialization/03-Project-Hangman/README.md
@@ -2,8 +2,8 @@
 
 ## Goals
 - [&check;] Download Google dictionary file
-- [ ] Load dictionary and select random 5-12 character word at game start
-- [ ] Indicate correct guesses and misses
-- [ ] On each turn allow the player to guess a letter that is case insensitive and update the display.
+- [&check;] Load dictionary and select random 5-12 character word at game start
+- [&check;] Indicate correct guesses and misses
+- [&check;] On each turn allow the player to guess a letter that is case insensitive and update the display.
 - [ ] Add ability to save the game instead of guessing a letter.
 - [ ] Add ability to load a game or play a new one.
--- Full-Stack/01-Ruby/05-Files-and-Serialization/03-Project-Hangman/lib/hangman.rb
+++ Full-Stack/01-Ruby/05-Files-and-Serialization/03-Project-Hangman/lib/hangman.rb
@@ -1,1 +1,61 @@
 # frozen_string_literal: true
+
+def create_secret_word
+  dictionary = File.readlines('word_list.txt', chomp: true)
+  dictionary.select { |word| word.length > 5 && word.length < 12 }.sample.split('')
+end
+
+def update_display(hits, misses)
+  print "#{hits}\n"
+  puts 'misses'
+  print "#{misses}\n"
+end
+
+def enter_user_guess
+  print 'Enter your guess: '
+  gets.chomp.downcase
+end
+
+def check_guess(secret_word, user_guess, hits, misses)
+  match = false
+  secret_word.each_with_index do |letter, index|
+    if user_guess == letter
+      hits[index] = letter
+      match = true
+    end
+  end
+
+  misses.push(user_guess) unless match
+end
+
+def check_win(hits)
+  if hits.none? { |letter| letter == '_' }
+    puts 'You win'
+  else
+    puts 'You lose'
+  end
+end
+
+def play_game
+  secret_word = create_secret_word
+  hits = Array.new(secret_word.length, '_')
+  misses = []
+
+  while misses.length < 6
+    user_guess = enter_user_guess
+    check_guess(secret_word, user_guess, hits, misses)
+    update_display(hits, misses)
+    break if hits.none?('_')
+  end
+
+  check_win(hits)
+end
+
+play_game
+
+#       _____
+#       |   |
+#       |   O
+#       |  /|\
+#       |  / \
+#  _____|______
