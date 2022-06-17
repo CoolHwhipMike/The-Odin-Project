046e326f2d6d7be057ce73b59cc7ab99a1083b2d
CoolHwhipMike <coolhwhipmike@gmail.com>
2022-06-17 14:28:00 -0400

    finally go enumerable my_each to work

--- Full-Stack/01-Ruby/06-Advanced-Ruby/03-Project-Custom-Enumerables/lib/my_enumerables.rb
+++ Full-Stack/01-Ruby/06-Advanced-Ruby/03-Project-Custom-Enumerables/lib/my_enumerables.rb
@@ -1,11 +1,20 @@
+# frozen_string_literal: true
+
+# all methods
 module Enumerable
   # Your code goes here
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
+# array class
 class Array
-  # Define my_each here
+  def my_each
+    index = 0
+
+    while index < length
+      yield self[index]
+      index += 1
+    end
+
+    self
+  end
 end
