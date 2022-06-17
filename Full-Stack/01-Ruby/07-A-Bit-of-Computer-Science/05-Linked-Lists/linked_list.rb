b58ef4a1fa25ac2fb41d301f5b17929e9a929219
CoolHwhipMike <coolhwhipmike@gmail.com>
2022-06-17 12:23:07 -0400

    followed along with linked list tutorial

--- Full-Stack/01-Ruby/07-A-Bit-of-Computer-Science/05-Linked-Lists/linked_list.rb
+++ Full-Stack/01-Ruby/07-A-Bit-of-Computer-Science/05-Linked-Lists/linked_list.rb
@@ -1,33 +1,84 @@
 # frozen_string_literal: true
 
-# # Class that represents the entire linked list
-# class LinkedList
-#   attr_accessor :head
+# class for the list
+class LinkedList
+  attr_accessor :head
 
-#   def initialize
-#     self.head = nil
-#   end
+  # Creates the initial empty linked list
+  def initialize
+    self.head = nil
+  end
 
-#   def append(value)
-#     if self.head.nil?
-#       self.head = Node.new(value)
-#     else
-#       last_node = self.head
-#       while (!last_node.next_node.nil?)
-#         last_node = last_node.next_node
-#       end
+  # Adds a value to the end of the list
+  def append(value)
+    if head.nil?
+      self.head = Node.new(value, nil)
+    else
+      last_node = head
+      # while (!last_node.next_node.nil?)
+      last_node = last_node.next_node until last_node.next_node.nil?
+      last_node.next_node = Node.new(value, nil)
+    end
+  end
 
-#       last_node.next_node = Node.new(value, nil)
-#     end
-#   end
+  # Adds a value to the beginning of the list
+  def prepend(value)
+    if head.nil?
+      self.head = Node.new(value, nil)
+    else
+      old_head = head
+      self.head = Node.new(value, old_head)
+    end
+  end
 
-#   # Class that creates the individual node for the linked list
-#   class Node
-#     attr_accessor :value, :next_node
+  # Removes a value from the list
+  def remove(value)
+    reutrn if head.nil?
 
-#     def initialize(value = nil, next_node = nil)
-#       self.value = value
-#       self.next_node = next_node
-#     end
-#   end
-# end
+    node = head
+    previous_node = nil
+    until node.nil?
+      if node.value == value
+        if !previous_node.nil?
+          previous_node.next_node = node.next_node
+        else
+          self.head = nil
+        end
+        return
+      end
+      previous_node = node
+      node = node.next_node
+    end
+  end
+
+  # Finds a value in the list
+  def find(value)
+    node = head
+    until node.nil?
+      return true if node.value == value
+
+      node = node.next_node
+    end
+
+    false
+  end
+
+  # class for each node
+  class Node
+    attr_accessor :value, :next_node
+
+    def initialize(value, next_node)
+      self.value = value
+      self.next_node = next_node
+    end
+  end
+end
+
+ll = LinkedList.new
+ll.append(10)
+ll.append(20)
+ll.prepend(30)
+
+puts ll.head.value
+puts ll.head.next_node.value
+p ll
---
+++ Full-Stack/01-Ruby/09-Testing-Ruby-with-RSpec/01-Test-Driven-Development/knowledge-check/knowledge-check.md
@@ -0,0 +1,8 @@
+## What is the “red-green-refactor” cycle?
+Writing code to get you test test pass. Then refactoring and using the test to make sure functionality hasn't changed.
+
+## What is the difference between unit and integration tests?
+Units test check that individual parts do what they're supposed to. Integration tests make sure the whole system works and simulates a user using the system.
+
+## How does TDD help in code refactoring?
+You can make changes and be sure that the functionality of trhe code isn't changing.
